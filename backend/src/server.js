6a45bb9e18d39fec44087034a722533b0552da07
sumeghna <sumeghna06@gmail.com>
2025-12-22 09:24:34 +0530

    feat: product schema and stable server setup

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const dotenv = require("dotenv");
+
+// FORCE dotenv to load from backend/.env
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+const connectDB = require("./config/db");
+const app = require("./app");
+
+connectDB();
+
+require("./models/product.model");
+console.log("Product model loaded");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
