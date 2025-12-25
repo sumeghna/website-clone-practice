ceeaea1b26c1e846963b3b4587fbab0564fcbc48
chiggawigga-max <akshath.bijapur.s111@kalvium.community>
2025-12-25 11:15:42 +0530

    added new images for the product cards

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 function Navbar() {
   return (
-    <nav>
-      <h2>Campus</h2>
-      <ul>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
-        
-      </ul>
-    </nav>
+    <><header className="navbar">
+      <div className="navbar-left">
+
+      </div>
+    </header><nav>
+        <h2>Campus</h2>
+        <ul>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+
+        </ul>
+      </nav></>
   );
 }
 
